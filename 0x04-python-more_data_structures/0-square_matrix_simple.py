be35ae444b1197443ee72e3fe7462f706896905c
Uchechi Eunice Owunne <93526806+Euniccodes@users.noreply.github.com>
2022-12-08 03:43:07 +0100

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 
-def square_matrix_simple(matrix=[]):    
+def square_matrix_simple(matrix=[]):
     """
-Square each element of matrix
-Args:
- matrix - a 2d lists
+    Square each element of matrix
+    Args:
+        matrix - a 2d lists
     """
-if matrix is None:
-
-return None    
+    if matrix is None:
+        return None
     return [
-    
         [x**2 for x in row] for row in matrix
-        
     ]
 
 
 if __name__ == '__main__':
-    
     s = square_matrix_simple([
-        [1, 2, 3], 
+        [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
     ])
